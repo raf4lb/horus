d4c5ee79045b17cc9f7e6864e3d03195d40f2eeb
Rafael Moreira Albuquerque <rafael.mor.alb@gmail.com>
2021-05-30 12:29:35 -0300

    Implementação de paginação

--- horus_challenge/settings.py
+++ horus_challenge/settings.py
@@ -148,7 +148,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5,
+    'PAGE_SIZE': 10,
 }
 
 # APPEND_SLASH = False
